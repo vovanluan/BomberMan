c938bd6dd9eefc67c00b382a20c9d7d9ed47b81f
Nguyen Duc Tri <ductriuv@gmail.com>
2015-11-08 21:02:38 +0700

    Temp

--- client/src/main.js
+++ client/src/main.js
@@ -8,7 +8,7 @@ function preload() {
     
 	//Next we load the tilesets
 	game.load.image('tiles', 'assets/tilemaps/tiles/tiles.png');
-    
+    game.load.image('player', 'assets/star.png');
 }
 
 var map;
@@ -17,18 +17,63 @@ var cursors;
 var player;
 
 function create() {
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
     //  Because we're loading CSV map data we have to specify the tile size here or we can't render it
 	map = game.add.tilemap('level1', 16, 16);
-
     //  Now add in the tileset
     map.addTilesetImage('tiles');
 
-    //  Creates our layer
     layer = map.createLayer(0);
+
+    map.setCollisionByExclusion([196], true, layer);
+    
 
     //  This resizes the game world to match the layer dimensions
     layer.resizeWorld();
+
+    player = game.add.sprite(100, 100, 'player', 1);
+    game.physics.arcade.enable(player);
+    player.body.setSize(10, 14, 2, 1);
+    player.body.collideWorldBounds = true;
+
+    cursors = game.input.keyboard.createCursorKeys();
+    console.log(player);
+    console.log(layer);
 }
 
 function update() {
+
+    game.physics.arcade.collide(player, layer);
+    player.body.velocity.x = 0;
+    player.body.velocity.y = 0;
+
+    if (cursors.right.isDown)
+    {
+        //  Move to the left
+        player.body.velocity.x = +150;
+
+        //player.animations.play('left');
+    }
+    if (cursors.left.isDown)
+    {
+        //  Move to the left
+        player.body.velocity.x = -150;
+
+        //player.animations.play('left');
+    }
+    if (cursors.up.isDown)
+    {
+        //  Move to the left
+        player.body.velocity.y = -150;
+
+        //player.animations.play('left');
+    }
+    if (cursors.down.isDown)
+    {
+        //  Move to the left
+        player.body.velocity.y = +150;
+
+        //player.animations.play('left');
+    }
 }
