f9018a50cc44985e5677f5f3ca88bc0abdc7012c
vovanluan <vovanluan201294@gmail.com>
2015-11-08 22:39:07 +0700

    Add movement

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,8 @@
-function BomberMan(Id, game){
+/*function BomberMan(Id, game){
     this.Id = Id;
     this.game = game;
     this.bomberman = game.add.sprite(40, 40, 'bomberman');
-}
+}*/
 var game = new Phaser.Game(600, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
@@ -15,7 +15,7 @@ function preload() {
 	game.load.image('tiles', 'assets/tilemaps/tiles/tileset.png');
 
     //Load spritesheet
-    game.load.spritesheet('bomberman', 'assets/images/Bomberman2.png', 34, 42);
+    game.load.spritesheet('bomberman', 'assets/images/Bomberman.png', 34, 42, 43, 0, 26);
     game.load.spritesheet('items', 'assets/images/Items.png');
     game.load.spritesheet('bombexplosion', 'assets/images/Bomb and Explosions.png');
 
@@ -28,6 +28,8 @@ var cursors;
 var player;
 
 function create() {
+
+    game.physics.startSystem(Phaser.Physics.ARCADE);
     //  Because we're loading CSV map data we have to specify the tile size here or we can't render it
 	map = game.add.tilemap('level1', 40, 40);
 
@@ -40,10 +42,54 @@ function create() {
     //  This resizes the game world to match the layer dimensions
     layer.resizeWorld();
 
-    // Create Player
-    //player = new BomberMan(0 , game);
+    layer.debug = true;
+
+    map.setCollision([127, 135], true, layer, true);
+
+    //  Player
     player = game.add.sprite(40, 40, 'bomberman');
+    player.animations.add('left', [1, 16, 31], 10, true);
+    player.animations.add('right', [3, 18, 33], 10, true);
+    player.animations.add('up', [2, 17, 32], 10, true);
+    player.animations.add('down', [0, 15, 30], 10, true);
+
+    // http://phaser.io/docs/2.4.4/Phaser.Physics.Arcade.Body.html#setSize
+    // player.body.setSize(34, 42, 0, 0);
+
+    game.physics.enable(player);
+
+    cursors = game.input.keyboard.createCursorKeys();
 }
 
 function update() {
+    //game.physics.arcade.collide(player, layer);
+
+    player.body.velocity.set(0);
+
+    if (cursors.left.isDown)
+    {
+        player.body.velocity.x = -100;
+        player.play('left');
+    }
+    else if (cursors.right.isDown)
+    {
+        player.body.velocity.x = 100;
+        player.play('right');
+    }
+    else if (cursors.up.isDown)
+    {
+        player.body.velocity.y = -100;
+        player.play('up');
+    }
+    else if (cursors.down.isDown)
+    {
+        player.body.velocity.y = 100;
+        player.play('down');
+    }
+    else
+    {
+        player.animations.stop();
+    }
+}
+function render() {
 }
