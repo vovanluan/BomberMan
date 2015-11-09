c31e6e572ab8b275bb9730af8d9633f5e537f763
Nguyen Duc Tri <ductriuv@gmail.com>
2015-11-09 10:38:25 +0700

    Temp

---
+++ server/server.js
@@ -0,0 +1,22 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection',function(socket){
+	console.log('a user connected');
+	socket.on('update', function(msg){
+	    console.log('message: ' + msg);
+	    io.emit("notify",msg);
+  	});
+	socket.on('disconnect',function () {
+		console.log("user disconnect");
+	})
+})
+
+http.listen(3001, function(){
+  console.log('listening on *:3001');
+});
