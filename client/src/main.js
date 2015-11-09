b75f72b9584a737cd2fe8b8a469f3f2be18639b5
vovanluan <vovanluan201294@gmail.com>
2015-11-09 19:47:55 +0700

    Edit map and add enemies

--- client/src/main.js
+++ client/src/main.js
@@ -3,7 +3,13 @@
     this.game = game;
     this.bomberman = game.add.sprite(40, 40, 'bomberman');
 }*/
-var game = new Phaser.Game(600, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+function Enemy(Id, game, x, y) {
+    this.Id = Id;
+    this.game = game;
+    this.enemy = game.add.sprite(x, y, 'easyenemies');
+}
+var game = new Phaser.Game(1000, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
 	// Tilemaps are split into two parts: the actual map data and the tilesets to render the map
@@ -21,6 +27,7 @@ function preload() {
 
     game.load.spritesheet('items', 'assets/images/Items.png');
     game.load.spritesheet('bombexplosion', 'assets/images/Bomb&Explosions.png');
+    game.load.spritesheet('easyenemies', 'assets/images/Enemies.png', 36, 32, 16, 0, 24);
 }
 
 var map;
@@ -28,6 +35,7 @@ var layer;
 var cursors;
 var player;
 var booms;
+var easyenemies;
 
 function create() {
 
@@ -48,8 +56,20 @@ function create() {
     //  This resizes the game world to match the layer dimensions
     layer.resizeWorld();
 
+    // easyenemies
+    easyenemies = game.add.group();
+    easyenemies.enableBody = true;
 
+    var enemy = easyenemies.create(200, 40, 'easyenemies');
+    enemy.body.gravity.x = 30;
+    enemy.body.bounce.x = 1;
+    enemy = easyenemies.create(240, 200, 'easyenemies');
+    enemy.body.velocity.y = 30;
 
+    // Create animations for all easyenemies in this group
+    easyenemies.callAll('animations.add', 'animations', 'walk', [0, 1, 2], 5, false);
+
+    // Player
     player = game.add.sprite(40, 40, 'bomberman');
     game.physics.arcade.enable(player);
     player.body.setSize(40, 40, 0, 0);
@@ -64,18 +84,20 @@ function create() {
     booms.enableBody = true;
     var boom = booms.create(0, game.world.height - 64, 'bomberman', 10);
 
-
     layer.debug = true;
-
-
 
     cursors = game.input.keyboard.createCursorKeys();
 }
 
 function update() {
 
-
+    game.physics.arcade.collide(easyenemies, layer);
     game.physics.arcade.collide(player, layer);
+
+    // Easy enemies
+    easyenemies.callAll('play', null, 'walk');
+    easyenemies.forEach(easyEnemyMovement, this);
+
     player.body.velocity.x = 0;
     player.body.velocity.y = 0;
 
@@ -112,6 +134,16 @@ function update() {
         player.frame = 0;
 
     }
+}
+function easyEnemyMovement(easyEnemy) {
+/*    if (easyEnemy.body.touching.left  || easyEnemy.body.touching.right) {
+        easyEnemy.body.velocity.x *= -1;
+    }
+    else if (easyEnemy.body.touching.up || easyEnemy.body.touching.down) {
+        easyEnemy.body.velocity.y *= -1;
+    }*/
+
+
 }
 function render() {
 }
